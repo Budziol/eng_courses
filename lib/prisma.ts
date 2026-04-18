06bf5d7dec7b81f2f3ba40970a22482e498b8291
Budziol <zajetykazdykurwanick@gmail.com>
2026-04-18 03:45:58 +0200

    updated_prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -17,6 +17,6 @@ declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
-export const db = globalThis.prismaGlobal ?? prismaClientSingleton();
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = db;
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
